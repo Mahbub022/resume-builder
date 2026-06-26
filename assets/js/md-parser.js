176b3ab42e9280f80f394d6fce705e34dd26b8c8
Mahbub022 <126968142+Mahbub022@users.noreply.github.com>
2026-06-26 23:57:10 +0600

    Implement Markdown parser function
    Added a Markdown parser function to handle file imports.

---
+++ assets/js/md-parser.js
@@ -0,0 +1,27 @@
+/*
+==========================================
+Markdown Parser
+==========================================
+*/
+
+"use strict";
+
+/*
+Commit 004
+
+Responsibilities
+
+- Read uploaded .md file
+
+- Parse sections
+
+- Convert to Resume JSON
+
+- Refresh UI
+*/
+
+function importMarkdown(file) {
+
+    console.log(file);
+
+}
