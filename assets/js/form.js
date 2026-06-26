1b00ae366738d656e9bc7e92f7ff4d2fa7bb5ccf
Mahbub022 <126968142+Mahbub022@users.noreply.github.com>
2026-06-26 23:56:37 +0600

    Create form.js with module structure and comments
    Added initial structure and comments for the Form Module.

---
+++ assets/js/form.js
@@ -0,0 +1,24 @@
+/*
+==========================================
+Form Module
+==========================================
+*/
+
+"use strict";
+
+/*
+This module will later:
+
+- Generate sections
+- Generate cards
+- Validate input
+- Sync HTML <-> Resume JSON
+
+Commit 002 starts here.
+*/
+
+function initializeForm() {
+
+    console.log("Form Module Loaded");
+
+}
